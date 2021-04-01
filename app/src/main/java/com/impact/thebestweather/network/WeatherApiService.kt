b5eb10140b638435f9cbe9c3cd25bb5a414a2b73
Ilya Ponomaryov <ip287733@gmail.com>
2021-04-01 22:19:40 +0500

    11

--- app/src/main/java/com/impact/thebestweather/network/WeatherApiService.kt
+++ app/src/main/java/com/impact/thebestweather/network/WeatherApiService.kt
@@ -1,6 +1,9 @@
 package com.impact.thebestweather.network
 
+import com.impact.thebestweather.models.DailyWeather
+import kotlinx.coroutines.Deferred
+
 interface WeatherApiService {
 
-    fun getOneCallWeather()
+    fun getOneCallWeather(): Deferred<DailyWeather>
 }
