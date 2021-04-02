0d02979ee138ceffc5cbc4c9580da1e936c8760a
Ilya-2000 <ip287733@gmail.com>
2021-04-02 12:21:16 +0500

    WeatherApiService.kt wrote

--- app/src/main/java/com/impact/thebestweather/network/WeatherApiService.kt
+++ app/src/main/java/com/impact/thebestweather/network/WeatherApiService.kt
@@ -1,9 +1,33 @@
 package com.impact.thebestweather.network
 
 import com.impact.thebestweather.models.DailyWeather
+import com.impact.thebestweather.utils.Constant
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import kotlinx.coroutines.Deferred
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface WeatherApiService {
+    @GET("/data/2.5/onecall?")
+    fun getOneCallWeather(
+        @Query("lat")lat: Double,
+        @Query("lon")lon: Double,
+        @Query("exclude")exclude: String,
+        @Query("appid")appid: String
+    ): Deferred<DailyWeather>
 
-    fun getOneCallWeather(): Deferred<DailyWeather>
+
+    companion object {
+        fun create() : WeatherApiService {
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .baseUrl(Constant.BASE_URL)
+                .build()
+            return retrofit.create(WeatherApiService::class.java)
+        }
+    }
 }
---
+++ app/src/main/java/com/impact/thebestweather/utils/Constant.kt
@@ -0,0 +1,8 @@
+package com.impact.thebestweather.utils
+
+class Constant {
+    companion object {
+        const val BASE_URL = "https://api.openweathermap.org"
+        const val API_KEY = "170c0bf20902da1de58b0227f138bd5b"
+    }
+}
