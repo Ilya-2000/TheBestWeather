84625a209febd1dcfaaa0e4744a7488e5d7ea735
Ilya Ponomaryov <ip287733@gmail.com>
2021-04-04 00:39:42 +0500

    12

--- app/src/main/java/com/impact/thebestweather/network/WeatherApiService.kt
+++ app/src/main/java/com/impact/thebestweather/network/WeatherApiService.kt
@@ -2,9 +2,10 @@ package com.impact.thebestweather.network
 
 import com.impact.thebestweather.models.DailyWeather
 import com.impact.thebestweather.utils.Constant
-import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import io.reactivex.Observable
 import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 import retrofit2.http.GET
@@ -17,14 +18,14 @@ interface WeatherApiService {
         @Query("lon")lon: Double,
         @Query("exclude")exclude: String,
         @Query("appid")appid: String
-    ): Deferred<DailyWeather>
+    ): Observable<DailyWeather>
 
 
     companion object {
         fun create() : WeatherApiService {
             val retrofit = Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .baseUrl(Constant.BASE_URL)
                 .build()
             return retrofit.create(WeatherApiService::class.java)
