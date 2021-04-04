f0f407e02eba64f414e9635437058721ef265820
Ilya Ponomaryov <ip287733@gmail.com>
2021-04-04 23:43:40 +0500

    00

--- app/src/main/java/com/impact/thebestweather/data/WeatherSource.kt
+++ app/src/main/java/com/impact/thebestweather/data/WeatherSource.kt
@@ -1,16 +1,24 @@
 package com.impact.thebestweather.data
 
+import com.impact.thebestweather.models.DailyWeather
 import com.impact.thebestweather.models.WeatherRequest
 import com.impact.thebestweather.network.WeatherApiService
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
-class WeatherSource {
+class WeatherSource(compositeDisposable: CompositeDisposable) {
     private val TAG = "WeatherSource"
     private val weatherApiService by lazy {
         WeatherApiService.create()
     }
 
-    suspend fun getWeather(weatherRequest: WeatherRequest) {
+    /*fun getWeather(weatherRequest: WeatherRequest) {
         weatherApiService.getOneCallWeather(weatherRequest.lat, weatherRequest.lon,
             weatherRequest.exclude, weatherRequest.appid)
-    }
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Observable<DailyWeather>())
+    }*/
 }
