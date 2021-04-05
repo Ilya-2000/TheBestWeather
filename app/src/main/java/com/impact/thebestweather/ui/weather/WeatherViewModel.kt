59afe94060b3ac93c072b0740dd0b4f907590555
Ilya-2000 <ip287733@gmail.com>
2021-04-05 14:17:33 +0500

    get request weather v4

--- app/src/main/java/com/impact/thebestweather/ui/weather/WeatherViewModel.kt
+++ app/src/main/java/com/impact/thebestweather/ui/weather/WeatherViewModel.kt
@@ -3,11 +3,21 @@ package com.impact.thebestweather.ui.weather
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.impact.thebestweather.data.repository.WeatherRepository
+import com.impact.thebestweather.models.WeatherRequest
+import com.impact.thebestweather.utils.Constant
+import io.reactivex.disposables.CompositeDisposable
 
 class WeatherViewModel : ViewModel() {
+    private val TAG = "WeatherViewModel"
+    private val compositeDisposable = CompositeDisposable()
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+
+    fun getWeather() {
+        WeatherRepository.getWeather(compositeDisposable, WeatherRequest(51.2305015, 58.4738015,
+            "hourly, daily",
+            Constant.API_KEY ))
     }
-    val text: LiveData<String> = _text
+
+
 }
