ad6b857662abbcbc52271f320c740b441aa1bb1e
Ilya-2000 <ip287733@gmail.com>
2021-04-05 12:30:52 +0500

    get request weather v1

--- app/src/main/java/com/impact/thebestweather/data/WeatherSource.kt
+++ app/src/main/java/com/impact/thebestweather/data/WeatherSource.kt
@@ -1,24 +1,40 @@
 package com.impact.thebestweather.data
 
+import android.util.Log
 import com.impact.thebestweather.models.DailyWeather
 import com.impact.thebestweather.models.WeatherRequest
 import com.impact.thebestweather.network.WeatherApiService
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableObserver
 import io.reactivex.schedulers.Schedulers
 
-class WeatherSource(compositeDisposable: CompositeDisposable) {
+class WeatherSource(private val compositeDisposable: CompositeDisposable) {
     private val TAG = "WeatherSource"
     private val weatherApiService by lazy {
         WeatherApiService.create()
     }
 
-    /*fun getWeather(weatherRequest: WeatherRequest) {
-        weatherApiService.getOneCallWeather(weatherRequest.lat, weatherRequest.lon,
-            weatherRequest.exclude, weatherRequest.appid)
+    fun getWeather(weatherRequest: WeatherRequest) {
+        compositeDisposable.add(weatherApiService.getOneCallWeather(weatherRequest.lat, weatherRequest.lon,
+                weatherRequest.exclude, weatherRequest.appid)
                 .observeOn(Schedulers.io())
                 .subscribeOn(AndroidSchedulers.mainThread())
-                .subscribe(object : Observable<DailyWeather>())
-    }*/
+                .subscribeWith(object : DisposableObserver<DailyWeather>(){
+                    override fun onNext(t: DailyWeather) {
+                        Log.d(TAG, "onNext: $t")
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Log.d(TAG, "onError: $e")
+                    }
+
+                    override fun onComplete() {
+                        Log.d(TAG, "onComplete")
+                    }
+
+                })
+        )
+    }
 }
