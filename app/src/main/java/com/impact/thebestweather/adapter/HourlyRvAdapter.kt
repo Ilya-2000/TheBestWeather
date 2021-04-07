1be4c99139c4380ad52fcb13536d09259795fe3e
Ilya-2000 <ip287733@gmail.com>
2021-04-07 13:55:39 +0500

    Resource.kt

--- app/src/main/java/com/impact/thebestweather/adapter/HourlyRvAdapter.kt
+++ app/src/main/java/com/impact/thebestweather/adapter/HourlyRvAdapter.kt
@@ -1,4 +1,25 @@
 package com.impact.thebestweather.adapter
 
-class HourlyRvAdapter {
-}
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/*class HourlyRvAdapter(): RecyclerView.Adapter<HourlyRvAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+}*/
