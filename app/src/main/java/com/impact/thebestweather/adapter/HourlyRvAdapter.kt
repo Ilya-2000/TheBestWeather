f07433f4d8385743613347885e5a327748377b1b
Ilya-2000 <ip287733@gmail.com>
2021-04-08 16:26:18 +0500

    bind v3

--- app/src/main/java/com/impact/thebestweather/adapter/HourlyRvAdapter.kt
+++ app/src/main/java/com/impact/thebestweather/adapter/HourlyRvAdapter.kt
@@ -1,25 +1,34 @@
 package com.impact.thebestweather.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.impact.thebestweather.R
+import com.impact.thebestweather.databinding.HourlyWeatherItemBinding
 
-/*class HourlyRvAdapter(): RecyclerView.Adapter<HourlyRvAdapter.ViewHolder>() {
+class HourlyRvAdapter(): RecyclerView.Adapter<HourlyRvAdapter.ViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
+        val view: HourlyWeatherItemBinding = DataBindingUtil
+                .inflate(LayoutInflater.from(parent.context),
+                        R.layout.hourly_weather_item, parent, false)
+        return ViewHolder(view)
     }
+
+
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
     }
 
-    override fun getItemCount(): Int {
+    override fun getItemCount(): Int = 24
+
+
+
+    inner class ViewHolder(itemView: HourlyWeatherItemBinding): RecyclerView.ViewHolder(itemView.root) {
 
     }
-
-    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
-    }
-}*/
+}
