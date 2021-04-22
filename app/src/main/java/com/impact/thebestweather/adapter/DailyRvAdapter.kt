0a83c5f25e8deceacab14127a9aa5980be5105b7
Ilya-2000 <ip287733@gmail.com>
2021-04-22 15:56:42 +0500

    daily v1

--- app/src/main/java/com/impact/thebestweather/adapter/DailyRvAdapter.kt
+++ app/src/main/java/com/impact/thebestweather/adapter/DailyRvAdapter.kt
@@ -1,4 +1,28 @@
 package com.impact.thebestweather.adapter
 
-class DailyRvAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.impact.thebestweather.databinding.DailyItemBinding
+import com.impact.thebestweather.models.weather.daily.DailyForecast
+import com.impact.thebestweather.ui.weather.WeatherViewModel
+
+class DailyRvAdapter(private var viewModel: WeatherViewModel): RecyclerView.Adapter<DailyRvAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class ViewHolder(var dailyItemBinding: DailyItemBinding): RecyclerView.ViewHolder(dailyItemBinding.root) {
+        fun bind(item: DailyForecast) {
+
+        }
+    }
 }
