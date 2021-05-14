386572ec1c0c7cc85aab179d604c677174ac8f88
Ilya-2000 <ip287733@gmail.com>
2021-05-14 13:32:22 +0500

    city v12

--- app/src/main/java/com/impact/thebestweather/ui/city/CityViewModel.kt
+++ app/src/main/java/com/impact/thebestweather/ui/city/CityViewModel.kt
@@ -3,9 +3,25 @@ package com.impact.thebestweather.ui.city
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.impact.thebestweather.data.CitySource
+import com.impact.thebestweather.models.location.LocationRequest
+import com.impact.thebestweather.utils.Constant
+import io.reactivex.disposables.CompositeDisposable
+import java.lang.Exception
 
 class CityViewModel : ViewModel() {
     private val TAG = "CityViewModel"
+    private val compositeDisposable = CompositeDisposable()
+    lateinit var citySource: CitySource
+
+
+    fun searchCity(text: String) {
+        try {
+            citySource.searchCity(compositeDisposable, LocationRequest(Constant.API_KEY, text, "en", "false"))
+        } catch (e: Exception) {
+
+        }
+    }
 
 
 
