3dbeba36fdcda58c55c64ed9365b6f134075c745
Ilya-2000 <ip287733@gmail.com>
2021-05-25 14:39:47 +0500

    SettingsFragment.kt

--- app/src/main/java/com/impact/thebestweather/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/impact/thebestweather/ui/settings/SettingsFragment.kt
@@ -1,26 +1,12 @@
 package com.impact.thebestweather.ui.settings
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.preference.PreferenceFragmentCompat
 import com.impact.thebestweather.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    private lateinit var settingsViewModel: SettingsViewModel
-
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        settingsViewModel =
-                ViewModelProvider(this).get(SettingsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_settings, container, false)
-
-        return root
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
 }
