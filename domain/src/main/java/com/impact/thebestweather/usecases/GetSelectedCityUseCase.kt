65d934be25e89dc9ef57c66ed818f71abf2f9802
Ilya-2000 <51691233+Ilya-2000@users.noreply.github.com>
2021-12-09 23:07:34 +0300

    GetSelectedCityUseCase.kt rename

--- domain/src/main/java/com/impact/thebestweather/usecases/GetSelectedCityUseCase.kt
+++ domain/src/main/java/com/impact/thebestweather/usecases/GetSelectedCityUseCase.kt
@@ -3,6 +3,6 @@ package com.impact.thebestweather.usecases
 import com.impact.thebestweather.CityRepository
 import com.impact.thebestweather.models.location.LocationItem
 
-class SelectCityUseCase(private val cityRepository: CityRepository) {
+class GetSelectedCityUseCase(private val cityRepository: CityRepository) {
     fun execute(data: LocationItem) = cityRepository.selectCity(locationItem = data)
 }
