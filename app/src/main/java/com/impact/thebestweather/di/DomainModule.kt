21bebc39229361d44f2f2567f1517dd11f4d8fbe
Ilya Ponomaryov <i.ponomaryov.00@gmail.com>
2021-12-10 11:47:09 +0500

    installIn added

--- app/src/main/java/com/impact/thebestweather/di/DomainModule.kt
+++ app/src/main/java/com/impact/thebestweather/di/DomainModule.kt
@@ -8,8 +8,11 @@ import com.impact.thebestweather.usecases.GetWeatherUseCase
 import com.impact.thebestweather.usecases.SetSelectCityUseCase
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
 
 @Module
+@InstallIn(ViewModelComponent::class)
 class DomainModule {
     @Provides
     fun provideGetWeatherUseCase(weatherRepository: WeatherRepository)
