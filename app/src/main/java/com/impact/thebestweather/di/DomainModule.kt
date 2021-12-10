83690bdea3f0d19725b72a1edba91bec946fa8c6
Ilya Ponomaryov <i.ponomaryov.00@gmail.com>
2021-12-10 11:26:11 +0500

    DomainModule.kt created

---
+++ app/src/main/java/com/impact/thebestweather/di/DomainModule.kt
@@ -0,0 +1,29 @@
+package com.impact.thebestweather.di
+
+import com.impact.thebestweather.CityRepository
+import com.impact.thebestweather.WeatherRepository
+import com.impact.thebestweather.usecases.GetCitiesUseCase
+import com.impact.thebestweather.usecases.GetSelectedCityUseCase
+import com.impact.thebestweather.usecases.GetWeatherUseCase
+import com.impact.thebestweather.usecases.SetSelectCityUseCase
+import dagger.Module
+import dagger.Provides
+
+@Module
+class DomainModule {
+    @Provides
+    fun provideGetWeatherUseCase(weatherRepository: WeatherRepository)
+    = GetWeatherUseCase(weatherRepository = weatherRepository)
+
+    @Provides
+    fun provideGetCitiesUseCase(cityRepository: CityRepository)
+    = GetCitiesUseCase(cityRepository = cityRepository)
+
+    @Provides
+    fun provideGetSelectedCityUseCase(cityRepository: CityRepository)
+    = GetSelectedCityUseCase(cityRepository = cityRepository)
+
+    @Provides
+    fun provideSetSelectedCityUseCase(cityRepository: CityRepository)
+    = SetSelectCityUseCase(cityRepository = cityRepository)
+}
