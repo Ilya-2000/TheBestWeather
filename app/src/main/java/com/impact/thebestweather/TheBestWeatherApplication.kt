e95c836aaad8d7767958c80c7fc50765399da1a1
Ilya Ponomaryov <i.ponomaryov.00@gmail.com>
2021-12-10 10:23:28 +0500

    TheBestWeatherApplication.kt created

---
+++ app/src/main/java/com/impact/thebestweather/TheBestWeatherApplication.kt
@@ -0,0 +1,9 @@
+package com.impact.thebestweather
+
+import android.app.Application
+import com.impact.thebestweather.usecases.GetWeatherUseCase
+import com.impact.thebestweather.weather.WeatherRepositoryImpl
+
+class TheBestWeatherApplication :  Application() {
+
+}
--- app/src/main/java/com/impact/thebestweather/ui/weather/WeatherFragment.kt
+++ app/src/main/java/com/impact/thebestweather/ui/weather/WeatherFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
@@ -30,6 +31,14 @@ class WeatherFragment : Fragment() {
     private val TAG = "WeatherFragment"
     private lateinit var weatherViewModel: WeatherViewModel
     private lateinit var navController: NavController
+
+    /*private val weatherViewModel: WeatherViewModel by viewModels {
+        BooksViewModel.BooksViewModelFactory(
+            ((requireActivity().application) as ).getBooksUseCase,
+            ((requireActivity().application) as CleanArchitectureBlueprintsApplication).getBookmarksUseCase,
+
+        )
+    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
