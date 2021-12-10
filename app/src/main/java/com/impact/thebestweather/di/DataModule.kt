1fd4067475d5c47ead56f8b55aaf1d538160c70e
Ilya Ponomaryov <i.ponomaryov.00@gmail.com>
2021-12-10 11:45:56 +0500

    singleton added

--- app/src/main/java/com/impact/thebestweather/di/DataModule.kt
+++ app/src/main/java/com/impact/thebestweather/di/DataModule.kt
@@ -8,17 +8,27 @@ import com.impact.thebestweather.weather.WeatherRemoteSource
 import com.impact.thebestweather.weather.WeatherRepositoryImpl
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
+@InstallIn(SingletonComponent::class)
 class DataModule {
+
     @Provides
+    @Singleton
     fun provideWeatherRepository(weatherRemoteSource: WeatherRemoteSource)
     = WeatherRepositoryImpl(weatherRemoteSource = weatherRemoteSource)
+
     @Provides
+    @Singleton
     fun provideLocationLocalStorage(@ApplicationContext context: Context)
     = LocationLocalStorageImpl(context = context)
+
     @Provides
+    @Singleton
     fun provideCityRepository(
         cityRemoteSource: CityRemoteSource,
         locationLocalStorageImpl: LocationLocalStorageImpl) = CityRepositoryImpl(
